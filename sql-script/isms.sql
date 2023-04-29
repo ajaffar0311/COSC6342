a092b8d58777eecdfd9da32f6f8dfec2c2824aef
ajaffar0311 <67613243+ajaffar0311@users.noreply.github.com>
2023-04-29 11:24:25 -0500

    Added tables for the users

--- sql-script/isms.sql
+++ sql-script/isms.sql
@@ -55,3 +55,80 @@ VALUES
 ('smith','ROLE_USER'),
 ('smith','ROLE_PROFESSOR'),
 ('smith','ROLE_ADMIN');
+
+CREATE DATABASE  IF NOT EXISTS `isms`;
+USE `isms`;
+
+--
+-- Table structure for table `studentusers`
+--
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `student`
+--
+
+INSERT INTO `student` VALUES
+	(1,'Leslie','Andrews','leslie@gmail.com'),
+	(2,'Emma','Baumgarten','emma@gmail.com'),
+	(3,'Avani','Gupta','avani@gmail.com'),
+	(4,'Yuri','Petrov','yuri@gmail.com'),
+	(5,'Juan','Vega','juan@gmail.com');
+
+--
+-- Table structure for table `professors`
+--
+
+DROP TABLE IF EXISTS `professor`;
+
+CREATE TABLE `professor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `professor`
+--
+
+INSERT INTO `professor` VALUES
+	(1,'Amjad','Nusayr','Nusayr@uhv.com'),
+	(2,'John','Smith','smith@uhv.com'),
+	(3,'Paris','Oak','oak@uhv.com'),
+	(4,'Poliack','Ash','ash@uhv.com'),
+	(5,'Hector','Hills','hector@uhv.com');
+
+    --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `admin`
+--
+
+INSERT INTO `admin` VALUES
+	(1,'Admin1','lastname1','Admin1@uhv.com'),
+	(2,'Admin2','lastname2','Admin2@uhv.com');
+
+
